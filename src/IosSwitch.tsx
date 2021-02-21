8f15bb7de704e746448dc326b1e9037507fa423b
Haïder Alshamma <haideralsh@gmail.com>
2021-02-20 21:55:48 -0500

    Add IosToggle
    I tried to to resemble to the best of my ability
    the switch outlined in the Apple guideline for
    human interface design.

---
+++ src/IosSwitch.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import BaseToggle, { ToggleProps } from './BaseToggle'
+
+// const trackHeight = 31.0
+// const trackRadius = trackHeight / 2.0
+// const trackInnerStart = trackHeight / 2.0
+// const trackInnerEnd = trackWidth - trackInnerStart
+// const trackInnerLength = trackInnerEnd - trackInnerStart
+
+const switchWidth = 59.0
+const switchHeight = 39.0
+
+const trackWidth = 51.0
+const trackHeight = 35.0
+
+const duration = 0.3 // ms
+const padding = (switchWidth - trackWidth) / 4 // (top, bottom, left, right)
+
+const IosToggle: React.FC<ToggleProps> = ({ value, ...rest }) => {
+  return (
+    <BaseToggle
+      animationDuration={duration}
+      endAnimationX={switchWidth - trackHeight - padding * 2}
+      trackCss={`
+        box-sizing: border-box;
+        display: inline-flex;
+        width: ${switchWidth}px;
+        height: ${switchHeight}px;
+        align-items: center;
+        
+        padding: ${padding}px; 
+        border-radius: 40px;
+        transition: background-color ${duration}s;
+        transition-property: background-color, box-shadow;
+        background-color: ${value ? 'rgb(52, 199, 89)' : 'transparent'};
+        box-shadow: ${
+          value ? 'none' : 'inset 0px 0px 0px 2px rgba(220, 220, 220, 0.5)'
+        };
+      `}
+      toggleCss={`
+      box-sizing: border-box;
+      background-color: #ffffff;
+      display: flex;
+      height: ${trackHeight}px;
+      width: ${trackHeight}px;
+      border-radius: 50%;
+      transition: box-shadow ${duration}s;
+      box-shadow: 0px 0px 2px rgb(0 0 0 / 30%), 0px 2px 4px rgb(0 0 0 / 20%);
+    `}
+      value={value}
+      {...rest}
+    />
+  )
+}
+
+export default IosToggle
