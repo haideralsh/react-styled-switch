0fb684118abca87b1d226406d7cb6c3f12683a9f
Haïder Alshamma <haideralsh@gmail.com>
2021-02-20 21:53:30 -0500

    Rename index.tsx to BaseToggle.tsx

--- src/BaseToggle.tsx
+++ src/BaseToggle.tsx
@@ -1,45 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
-import { css } from '@emotion/css'
+import { css, cx } from '@emotion/css'
 
 export type ToggleProps = {
   value?: boolean
   activeColor?: React.CSSProperties['color']
   trackColor?: React.CSSProperties['color']
   onChange?: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
-  dir?: 'ltr' | 'rtl'
-  disabled?: boolean
+  textDirection?: 'ltr' | 'rtl'
 }
 
 export type BaseToggleProps = {
   trackCss: string
   toggleCss: string
-  animationDuration: number
-  animationType: 'spring' // Offer more types
-  beginAnimationX: number
+  animationDuration: number // Maybe have a default value?
+  animationType?: 'spring' // Offer more types
+  beginAnimationX?: number
   endAnimationX: number
 }
 
+// Opacity of a disabled switch, as eye-balled from iOS Simulator on Mac.
+const disabledOpacity = 0.5
+
 const BaseToggle: React.FC<BaseToggleProps & ToggleProps> = ({
   value = false,
-  dir = 'ltr',
-  disabled = false,
+  // textDirection = 'ltr',
   onChange,
   trackCss,
   toggleCss,
   animationDuration,
-  animationType,
-  beginAnimationX,
+  animationType = 'spring',
+  beginAnimationX = 0,
   endAnimationX,
 }) => {
   const [animateX, setAnimateX] = useState(beginAnimationX)
+  const disabled = !Boolean(onChange)
+
+  console.log(value)
 
   useEffect(() => {
     setAnimateX(value ? endAnimationX : beginAnimationX)
   }, [value, endAnimationX, beginAnimationX])
 
   return (
-    <motion.span onClick={onChange} className={css(trackCss)}>
+    <motion.span
+      onClick={onChange}
+      className={cx(
+        css(trackCss),
+        css`
+          ${disabled && `opacity: ${disabledOpacity};`}
+        `
+      )}
+    >
       <motion.span
         animate={{ x: animateX }}
         transition={{ duration: animationDuration, type: animationType }}
