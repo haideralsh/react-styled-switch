4f63b8a054b69247fcf58a4ff84df1ced35d706e
Haïder Alshamma <haideralsh@gmail.com>
2021-03-04 20:42:24 -0500

    Add Windows Phone 7 light theme toggle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as IosToggle } from './variants/IosToggle'
 export { default as MacOsToggle } from './variants/MacOsToggle'
+export { default as WindowsPhone } from './variants/WindowsPhone'
 export { default as useToggle } from './hooks/useToggle'
