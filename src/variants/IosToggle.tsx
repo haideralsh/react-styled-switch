26bb88195b256f496b65c0ed040bf81a845e1840
Haïder Alshamma <haideralsh@gmail.com>
2021-03-25 21:17:08 -0400

    Use theme object to group properties for IosToggle

--- src/variants/IosToggle.tsx
+++ src/variants/IosToggle.tsx
@@ -1,40 +1,68 @@
 import React from 'react'
 import BaseToggle, { ToggleProps } from '../BaseToggle'
 
-const switchWidth = 59.0,
-  switchHeight = 39.0,
-  trackWidth = 51.0,
-  trackHeight = 35.0,
-  duration = 0.3, // seconds
-  padding = (switchWidth - trackWidth) / 4 // (4 sides: top, bottom, left, right)
+const theme = {
+  animation: {
+    duration: 0.3,
+  },
+  dimensions: {
+    track: {
+      width: 59,
+      height: 39,
+      borderRadius: 40,
+      padding: 2,
+    },
+    thumb: {
+      width: 35,
+      height: 35,
+      borderRadius: '50%',
+    },
+  },
+  palette: {
+    track: {
+      active: '#34c759',
+      inActive: '#e9e9ea',
+    },
+    thumb: {
+      background: '#ffffff',
+      shadow: '0px 2px 4px rgb(0 0 0 / 20%);',
+    },
+  },
+}
+
 const IosToggle: React.FC<ToggleProps> = ({ on, ...rest }) => {
+  const { animation, dimensions, palette } = theme
 
   return (
     <BaseToggle
-      animationDuration={duration}
-      endAnimationX={switchWidth - trackHeight - padding * 2}
+      animationDuration={animation.duration}
+      endAnimationX={
+        dimensions.track.width -
+        dimensions.thumb.width -
+        dimensions.track.padding * 2
+      }
       trackCss={`
         box-sizing: border-box;
         display: inline-flex;
-        width: ${switchWidth}px;
-        height: ${switchHeight}px;
+        width: ${dimensions.track.width}px;
+        height: ${dimensions.track.height}px;
         align-items: center;
-        padding: ${padding}px; 
-        border-radius: 40px;
-        transition: all ${duration}s;
+        padding: ${dimensions.track.padding}px; 
+        border-radius: ${dimensions.track.borderRadius}px;
+        transition: all ${animation.duration}s;
         transition-property: background-color, box-shadow;
-        background-color: ${value ? 'rgb(52, 199, 89)' : '#E9E9EA'};
+        background-color: ${on ? palette.track.active : palette.track.inActive};
       `}
       thumbCss={`
-      box-sizing: border-box;
-      background-color: #ffffff;
-      display: flex;
-      height: ${trackHeight}px;
-      width: ${trackHeight}px;
-      border-radius: 50%;
-      transition: box-shadow ${duration}s;
-      box-shadow: 0px 2px 4px rgb(0 0 0 / 20%);
-    `}
+        box-sizing: border-box;
+        background-color: ${palette.thumb.background};
+        display: flex;
+        height: ${dimensions.thumb.height}px;
+        width: ${dimensions.thumb.width}px;
+        border-radius: ${dimensions.thumb.borderRadius};
+        transition: box-shadow ${animation.duration}s;
+        box-shadow: ${palette.thumb.shadow};
+      `}
       on={on}
       {...rest}
     />
