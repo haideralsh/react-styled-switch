d458ce5ef1fd37e13671721cac987b6765d2f6d3
Haïder Alshamma <haideralsh@gmail.com>
2021-05-16 23:02:54 -0400

    Add react-styled-switch dependency

--- src/variants/WindowsPhoneSwitch.tsx
+++ src/variants/WindowsPhoneSwitch.tsx
@@ -1,21 +1,68 @@
 import React from 'react'
-import BaseSwitch, { SwitchProps } from '../BaseSwitch'
+import BaseSwitch, { SwitchProps, SwitchSize } from '../BaseSwitch'
 
-const theme = {
-  animation: {
-    duration: 0.3, // seconds
+type MaterialSwitchDimention = {
+  track: {
+    width: number
+    height: number
+    padding: number
+    borderWidth: number
+  }
+  thumb: {
+    width: number
+    height: number
+    borderGap: number
+  }
+}
+
+const dimensions: Record<SwitchSize, MaterialSwitchDimention> = {
+  small: {
+    track: {
+      width: 86 * 0.8,
+      height: 33 * 0.8,
+      padding: 0,
+      borderWidth: 2,
+    },
+    thumb: {
+      width: 20 * 0.8,
+      height: 37 * 0.8,
+      borderGap: 2,
+    },
   },
-  dimensions: {
+
+  medium: {
     track: {
       width: 86,
       height: 33,
       padding: 0,
+      borderWidth: 2,
     },
     thumb: {
       width: 20,
       height: 37,
       borderGap: 2,
     },
+  },
+
+  large: {
+    track: {
+      width: 86 * 1.5,
+      height: 33 * 1.5,
+      padding: 0,
+      borderWidth: 3,
+    },
+    thumb: {
+      width: 20 * 1.5,
+      height: 37 * 1.5,
+      borderGap: 3,
+    },
+  },
+}
+
+const theme = {
+  dimensions,
+  animation: {
+    duration: 0.3, // seconds
   },
   palette: {
     track: {
@@ -33,33 +80,41 @@ const theme = {
   },
 }
 
-const WindowsPhoneSwitch: React.FC<SwitchProps> = ({ on, ...rest }) => {
+const WindowsPhoneSwitch: React.FC<SwitchProps> = ({
+  on,
+  size = 'medium',
+  ...rest
+}) => {
   const { animation, dimensions, palette } = theme
 
   return (
     <BaseSwitch
       animationDuration={animation.duration}
-      endAnimationX={dimensions.track.width - dimensions.thumb.width}
+      endAnimationX={
+        dimensions[size].track.width - dimensions[size].thumb.width
+      }
       trackCss={`
         box-sizing: border-box;
         display: inline-flex;
         align-items: center;
-        width: ${dimensions.track.width}px;
-        height: ${dimensions.track.height}px;
-        padding: ${dimensions.track.padding}px; 
+        width: ${dimensions[size].track.width}px;
+        height: ${dimensions[size].track.height}px;
+        padding: ${dimensions[size].track.padding}px;
         box-shadow: inset 0 0 0 2px ${palette.track.shadow};
-        border: 2px solid ${palette.track.border};
+        border: ${dimensions[size].track.borderWidth}px solid ${
+        palette.track.border
+      };
         background-color: ${on ? palette.track.active : palette.track.inActive};
       `}
       thumbCss={`
       box-sizing: border-box;
       background-color: #ffffff;
       display: flex;
-      width: ${dimensions.thumb.width}px;
-      height: ${dimensions.thumb.height}px;
-      border: ${dimensions.thumb.borderGap}px solid #000;
-      box-shadow: 0px 0px 0px ${dimensions.thumb.borderGap}px #fff;
-      margin-left: -${dimensions.thumb.borderGap}px;
+      width: ${dimensions[size].thumb.width}px;
+      height: ${dimensions[size].thumb.height}px;
+      border: ${dimensions[size].thumb.borderGap}px solid #000;
+      box-shadow: 0px 0px 0px ${dimensions[size].thumb.borderGap}px #fff;
+      margin-left: -${dimensions[size].thumb.borderGap}px;
     `}
       on={on}
       {...rest}
